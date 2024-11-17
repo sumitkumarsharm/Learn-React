7b0fcef54609d202a032797a8febf0b16323fa73
Naman Sharma <snaman2748@gmail.com>
2024-11-18 01:03:07 +0530

    Learn how to Add all elmement in aray

--- 02_RunCountingApp/script.js
+++ 02_RunCountingApp/script.js
@@ -1,26 +1,30 @@
 let score = 0;
 let wicket = 0;
+let BallwiseRes = [];
 
-// referance handling
-function AddOne() {
-  score = score + 1;
-  Root.render(<App />);
-  console.log(score);
-}
+// // referance handling
+// function AddOne() {
+//   BallwiseRes.push()
+//   score = score + 1;
+//   Root.render(<App />);
+//   console.log(score);
+// }
 
-function AddTwo() {
-  score = score + 2;
-  // here i reRander the element
-  Root.render(<App />);
-  console.log(score);
-}
+// function AddTwo() {
+//   score = score + 2;
+//   // here i reRander the element
+//   Root.render(<App />);
+//   console.log(score);
+// }
 
 // event handling
 function AddScore(num) {
   if (wicket < 10) {
+    BallwiseRes.push(num);
     score = score + num;
     // here i reRander the element
     Root.render(<App />);
+    console.log(BallwiseRes);
   } else {
     alert("Your Target is", score);
   }
@@ -28,13 +32,66 @@ function AddScore(num) {
 
 function Addwicket() {
   if (wicket < 10) {
+    BallwiseRes.push('W');
     wicket = wicket + 1;
     // here i reRander the element
     Root.render(<App />);
-  }else{
+    console.log(BallwiseRes);
+    
+  } else {
     alert("You have already lose 10 wickets");
   }
 }
+
+const ScoreButtons = () => (
+  <div className="Run-Veeket">
+    <button
+      onClick={() => {
+        AddScore(1);
+      }}
+    >
+      1
+    </button>
+    <button
+      onClick={() => {
+        AddScore(2);
+      }}
+    >
+      2
+    </button>
+
+    {/*this is callled event handling and this is alos inline function */}
+    <button
+      onClick={() => {
+        AddScore(3);
+      }}
+    >
+      3
+    </button>
+    <button
+      onClick={() => {
+        AddScore(4);
+      }}
+    >
+      4
+    </button>
+    <button
+      onClick={() => {
+        AddScore(5);
+      }}
+    >
+      5
+    </button>
+    <button
+      onClick={() => {
+        AddScore(6);
+      }}
+    >
+      6
+    </button>
+    <button onClick={Addwicket}>Wicket</button>
+  </div>
+);
 
 // creating Element
 const App = () => {
@@ -44,57 +101,7 @@ const App = () => {
       <h2>
         Score : {score}/{wicket}
       </h2>
-      <div className="Run-Veeket">
-        {/* this two function is referance to the function  */}
-        {/* <button onClick={AddOne}>1</button> */}
-        {/* <button onClick={AddTwo}>2</button> */}
-
-        <button
-          onClick={() => {
-            AddScore(1);
-          }}
-        >
-          1
-        </button>
-        <button
-          onClick={() => {
-            AddScore(2);
-          }}
-        >
-          2
-        </button>
-
-        {/*this is callled event handling and this is alos inline function */}
-        <button
-          onClick={() => {
-            AddScore(3);
-          }}
-        >
-          3
-        </button>
-        <button
-          onClick={() => {
-            AddScore(4);
-          }}
-        >
-          4
-        </button>
-        <button
-          onClick={() => {
-            AddScore(5);
-          }}
-        >
-          5
-        </button>
-        <button
-          onClick={() => {
-            AddScore(6);
-          }}
-        >
-          6
-        </button>
-        <button onClick={Addwicket}>Wicket</button>
-      </div>
+      <ScoreButtons />
     </div>
   );
 };
